327d095cf60fc37656037324912b4657f48f2f4b
Gulshid <gulshidzada4@gmail.com>
2024-11-27 22:43:11 +0500

    Data Type

---
+++ data_type.py
@@ -0,0 +1,17 @@
+x=2
+y=3.2
+z='Hello'
+a=True
+b="World"
+c=['List']
+d=(1,2)
+e={"set"}
+# Here is the all data_type of python
+print("The value of x is ",x," and data type ",type(x))
+print("The value of y is ",y," and data type ",type(y))
+print("The value of z is ",z," and data type ",type(z))
+print("The value of a is ",a," and data type ",type(a))
+print("The value of b is ",b," and data type ",type(b))
+print("The value of c is ",c," and data type ",type(c))
+print("The value of d is ",d," and data type ",type(d))
+print("The value of e is ",e," and data type ",type(e))
