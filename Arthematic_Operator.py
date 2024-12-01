ec5e02b23b666f6f3e54dd6de78bc21562300275
Gulshid <gulshidzada4@gmail.com>
2024-12-01 18:40:31 +0500

    All Arthematic operator

---
+++ Arthematic_Operator.py
@@ -0,0 +1,13 @@
+#All Arthematic Operator of Operation
+print("Enter the value for x and y to operate all operation of arthematic:")
+print("Enter the value for x:")
+x=int(input())
+print("Enter the value for y:")
+y=int(input())
+
+#Arthematic operator are(+,_,*,/,%)
+print("The sum of x and y is:",x+y)
+print("The Subtraction of x and y is:",x-y)
+print("The Multiplication of x and y is:",x*y)
+print("The division of x and y is:",x/y)
+print("The Remainder of x and y is:",x%y)
