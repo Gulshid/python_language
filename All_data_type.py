6b139bd652a7d017d317c116fc3ea3591dc04917
Gulshid <gulshidzada4@gmail.com>
2024-12-01 18:24:09 +0500

    All type of data_type in python

---
+++ All_data_type.py
@@ -0,0 +1,35 @@
+#List Data_type
+List=[1,2,3,4,5,6]
+print(List)
+
+#Set Data_type
+Set={1,2,3,4,5,6}
+print(Set)
+
+#Tuple Data_type
+Tuple=(1,2,3,4,5,6)
+print(Tuple)
+
+#class dict Data_type
+dict={
+    'Hello':" World",
+    'Country':"pakistan",
+    'Capital':"Islamabad"
+}
+print(dict)
+
+#int Data_type
+inetger=230
+print(inetger)
+
+#float Data_type
+floating=23.233
+print(floating)
+
+#String Data_type
+String ="Hello World!"
+print(String)
+
+#bool Data_type
+bool=True
+print(bool)
