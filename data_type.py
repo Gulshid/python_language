6ff7094b30231c61f52d4558cb5ab273f31614c9
Gulshid <gulshidzada4@gmail.com>
2024-12-01 18:12:01 +0500

    update data_type

--- data_type.py
+++ data_type.py
@@ -6,6 +6,14 @@ b="World"
 c=['List']
 d=(1,2)
 e={"set"}
+dict={
+    'Name':"Gulshid Zada",
+    'Roll No':391,
+    'Section':"F",
+    "Batch":2023_2027,
+    "University":" University of Agriculture,Peshawar"
+    
+}
 # Here is the all data_type of python
 print("The value of x is ",x," and data type ",type(x))
 print("The value of y is ",y," and data type ",type(y))
@@ -15,3 +23,5 @@ print("The value of b is ",b," and data type ",type(b))
 print("The value of c is ",c," and data type ",type(c))
 print("The value of d is ",d," and data type ",type(d))
 print("The value of e is ",e," and data type ",type(e))
+print("The value of dict is ",dict," and data type ",type(dict))
+print("The Specific value  of dict variable is ",dict["Name"],dict["University"])
