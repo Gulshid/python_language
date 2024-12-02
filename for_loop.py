3268cda874489caf196e408809a09e819242d3fd
Gulshid <gulshidzada4@gmail.com>
2024-12-02 11:04:44 +0500

    for loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+for i  in range(10):
+    print(i)
+
+
+x=[1,2,3,4]
+for i in [x]:
+    print(i)
+
+#concatination of two string
+String="Hi this is For_loop"
+String_2=" HI !This is for_loop"
+for o in [String+String_2]:
+    print(o)
+    
+
+    
