04919aa7bdf9d0e58a0251938782c708d3212524
Gulshid <gulshidzada4@gmail.com>
2024-12-02 11:15:15 +0500

    while loop

--- while_loop.py
+++ while_loop.py
@@ -2,3 +2,14 @@ i=0
 while(i<=20):
     print("  Hello\n This is ",i,"number") #\n,\t,\r,\vetc(Escape Sequences)
     i+=1
+    
+    
+#table using while loop
+x=int(input("Enter the table that you  want value:"))
+y=int(input("Enter the table valule :"))
+i=1
+while(i<=y):
+
+        print(x,'X',i,"=",x*i)
+        i+=1
+   
