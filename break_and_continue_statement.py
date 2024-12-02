10006113628cefed2c3a4e726db9b46c1380eb15
Gulshid <gulshidzada4@gmail.com>
2024-12-02 11:32:35 +0500

    continue and break statement

---
+++ break_and_continue_statement.py
@@ -0,0 +1,15 @@
+for i in range(10):
+    if(i==6):
+        continue
+        
+        break
+    print(i)
+
+for i in range(10):
+    if(i!=6):
+        continue
+        
+        break
+    print(i)
+
+        
