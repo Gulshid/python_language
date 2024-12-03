d62cbfbe602fdb290cf1671600c35a6b5b681a01
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-03 21:51:14 +0500

    using class and object car class

---
+++ class_and_object.py
@@ -0,0 +1,25 @@
+#The class which is to find the car and model 
+class Car:
+    
+     
+                car_color="Green"
+                model='Bogatti'
+                plat_num=2301
+                cost=500000000000
+                print(car_color)
+                print(model)
+                print(plat_num)
+                print(cost)
+                    
+   
+        
+car_obj=Car()
+print("**********")
+print("Car Color:",car_obj.car_color)
+print("Car Name:",car_obj.model)
+print("Car Plate_number:",car_obj.plat_num)
+print("Car Cost:",car_obj.cost)
+
+        
+        
+        
