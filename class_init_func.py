83958128d18691c0eaac814e5ff092038d9171fc
Gulshid Zada <gulshidzada4@gmail.com>
2024-12-04 08:23:09 +0500

    using init function in class

---
+++ class_init_func.py
@@ -0,0 +1,11 @@
+class initfunction:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        print("The value of a is ",a)
+        print("The value of b is ",b)
+        
+
+init1=initfunction("Hello",39)
+print(init1)
+
